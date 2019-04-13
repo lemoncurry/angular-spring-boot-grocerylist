04163c0839f0a5b78c45fde27a118e88158b2330
lemoncurry <git@annakrueger.de>
2019-04-13 10:06:35 +0200

    remove GrListItem and GrList

--- src/main/java/com/lemoncurry/grocerylist/repository/GrListItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lemoncurry.grocerylist.repository;
-
-import com.lemoncurry.grocerylist.model.GrListItem;
-import com.lemoncurry.grocerylist.model.GrListItemPK;
-import org.springframework.data.repository.CrudRepository;
-
-public interface GrListItemRepository extends CrudRepository<GrListItem, GrListItemPK> {
-
-}
--- src/main/java/com/lemoncurry/grocerylist/repository/GrListRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lemoncurry.grocerylist.repository;
-
-import com.lemoncurry.grocerylist.model.GrList;
-import org.springframework.data.repository.CrudRepository;
-
-public interface GrListRepository extends CrudRepository<GrList, Long> {
-}
