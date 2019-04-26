698b98e60151170a7b982be356da8fe70bd436e9
lemoncurry <git@annakrueger.de>
2019-04-26 22:39:35 +0200

    create dev environment, use in-memory-db

--- src/angular-fe/src/app/app.module.ts
+++ src/angular-fe/src/app/app.module.ts
@@ -1,12 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule }    from '@angular/common/http';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { GroceryListComponent } from './grocery-list/grocery-list.component';
-import { ItemsComponent } from './grocery-list/items/component/items.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {environment} from "../environments/environment";
+import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {InMemoryDataService} from './in-memory-data.service';
+import {GroceryListComponent} from './grocery-list/grocery-list.component';
+import {ItemsComponent} from './grocery-list/items/items.component';
 
 @NgModule({
   declarations: [
@@ -18,9 +21,14 @@ import { ItemsComponent } from './grocery-list/items/component/items.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    environment.production ?
+      [] : HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, {dataEncapsulation: false}
+      )
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
